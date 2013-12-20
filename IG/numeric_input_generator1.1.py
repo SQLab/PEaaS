a792f431dd7f82cd41c15fd94315dd167531ecb9
CuteMonkey <b320enn8182006nee@yahoo.com.tw>
2013-12-20 13:35:13 +0800

    Upload IG

---
+++ IG/numeric_input_generator1.1.py
@@ -0,0 +1,400 @@
+#This version handles 4 types: %d, %f, %dl, %fl.
+#update item: Make value range limited.
+
+import io, random, re, sys
+from obj_types import *
+
+
+def check_rel(l_op, r_op, op_type):  #to check if the comparing relation is established
+	if op_type == '<':
+		if l_op < r_op:
+			return True
+		else:
+			return False
+	elif op_type == '>':
+		if l_op > r_op:
+			return True
+		else:
+			return False
+	elif op_type == '<=':
+		if l_op <= r_op:
+			return True
+		else:
+			return False
+	elif op_type == '>=':
+		if l_op >= r_op:
+			return True
+		else:
+			return False
+	elif op_type == '==':
+		if l_op == r_op:
+			return True
+		else:
+			return False
+	elif op_type == '!=':
+		if l_op != r_op:
+			return True
+		else:
+			return False
+
+
+while True:
+	try:
+		ifile_path = sys.argv[1]
+		ifile = open(ifile_path, 'r')
+		break
+	except OSError:  #if open file unsuccessfully
+		print('That was not a valid path. Please try again...')
+ofile_path = ifile_path[:len(ifile_path) - 4] + '_input.txt'
+ofile = open(ofile_path, 'w')
+
+level_num = sys.argv[2]  #easy, medium, hard(1, 2, 3)
+part_num = 1  #the number of the handling part
+itext = ifile.readline()
+ilen, ipos = len(itext), 0  #'ipos' is the start position of unread part of 'itext'
+input_num = -1  #-1 is nuset value
+orecord = list()  #to record the context which will write to 'ofile'
+type_record = []  #to store the type information of elements in 'orecord'
+obj_record = []  #to store objects in 'orecord'
+rel_record = []  #to store all relations of two vars
+end_str_record = list()  #to record the context of end-string
+lexical_error, been_match = False, False  #'been_match' checks if there is no match at each iteration of each part
+third_part_end, has_end_str = False, False
+
+#--analysis stage--
+while itext != '':
+	while ipos < ilen:
+		if part_num == 1:  #handle the 'number of input' section
+			re_result = re.match('%%', itext[ipos:])
+			if not been_match and re_result:
+				been_match = True
+				if input_num == -1:
+					input_num = 10  #the default value of 'input_num'
+				part_num = 2
+				ipos = ipos + 2
+			
+			re_result = re.match('Yes', itext[ipos:])
+			if not been_match and re_result:
+				been_match = True
+				if input_num == -1:
+					input_num = random.randint(1, 10)
+					ofile.write(str(input_num) + '\n')
+					ipos = ipos + 3
+				else:
+					lexical_error = True
+					break
+			
+			re_result = re.match('No', itext[ipos:])
+			if not been_match and re_result:
+				been_match = True
+				if input_num == -1:
+					input_num = 10
+					ipos = ipos + 2
+				else:
+					lexical_error = True
+					break
+			
+			re_result = re.match('\s+', itext[ipos:])
+			if not been_match and re_result:
+				been_match = True
+				ipos = ipos + len(re_result.group(0))
+		elif part_num == 2:  #handle the 1st part of the 'input format' section
+			re_result = re.match(r'{', itext[ipos:])
+			if not been_match and re_result:
+				been_match = True
+				part_num = 3
+				ipos = ipos + 1
+			
+			re_result = re.match('%dl', itext[ipos:])  #integer list case(not finish)
+			if not been_match and re_result:
+				been_match = True
+				ipos = ipos + 3
+			
+			re_result = re.match('%fl', itext[ipos:])  #float list case(not finish)
+			if not been_match and re_result:
+				been_match = True
+				ipos = ipos + 3
+			
+			re_result = re.match('%d', itext[ipos:])  #integer case
+			if not been_match and re_result:
+				been_match = True
+				orecord.append(VarInt())
+				obj_record.append(orecord[-1])
+				type_record.append(orecord[-1]._type)
+				ipos = ipos + 2
+			
+			re_result = re.match('%f', itext[ipos:])  #float case(not finish)
+			if not been_match and re_result:
+				been_match = True
+				ipos = ipos + 2
+			
+			re_result = re.match('"[^"\n]*"', itext[ipos:])  #string case
+			if not been_match and re_result:
+				been_match = True
+				re_str = re_result.group(0)
+				ipos = ipos + len(re_str)
+				re_str = re_str[1:len(re_str)-1]
+				re_pos = 0
+				while re_pos < len(re_str):  #analyze the content of the string
+					re_result = re.match(r'\\n', re_str[re_pos:])
+					if re_result:
+						orecord.append('\n')
+						type_record.append(None)
+						re_pos = re_pos + 2
+					
+					re_result = re.match(r'\\t', re_str[re_pos:])
+					if re_result:
+						orecord.append('\t')
+						type_record.append(None)
+						re_pos = re_pos + 2
+					
+					re_result = re.match('[ ]+', re_str[re_pos:])
+					if re_result:
+						orecord.append(re_result.group(0))
+						type_record.append(None)
+						re_pos = re_pos + len(re_result.group(0))
+					
+					re_result = re.match('[^\\\\]+', re_str[re_pos:])
+					if re_result:
+						orecord.append(re_result.group(0))
+						type_record.append(None)
+						re_pos = re_pos + len(re_result.group(0))
+			
+			re_result = re.match('\s+', itext[ipos:])  #space characters case
+			if not been_match and re_result:
+				been_match = True
+				ipos = ipos + len(re_result.group(0))
+		elif part_num == 3:  #handle the 2nd part of the 'input format' section(not finish)
+			re_result = re.match('%%', itext[ipos:])
+			if not been_match and re_result:
+				if third_part_end:
+					been_match = True
+					part_num = 4
+					ipos = ipos + 2
+				else:
+					lexical_error = True
+					break
+			
+			re_result = re.match(r'}', itext[ipos:])
+			if not been_match and re_result:
+				been_match, third_part_end = True, True
+				ipos = ipos + 1
+			
+			#case of '='
+			re_result = re.match('\$([0-9]+)[ ]*=[ ]*(-?[0-9]+)[ ]*;', itext[ipos:])
+			if not been_match and re_result:
+				been_match = True
+				obj_index = int(re_result.group(1)) - 1
+				r_op = int(re_result.group(2))
+				obj_record[obj_index].set_max(r_op)
+				obj_record[obj_index].set_min(r_op)
+				ipos = ipos + len(re_result.group(0))
+			
+			#case of '<'
+			re_result = re.match('\$([0-9]+)[ ]*<[ ]*(-?[0-9]+)[ ]*;', itext[ipos:])
+			if not been_match and re_result:
+				been_match = True
+				obj_index = int(re_result.group(1)) - 1
+				r_op = int(re_result.group(2))
+				obj_record[obj_index].set_max(r_op - 1)
+				ipos = ipos + len(re_result.group(0))
+			
+			re_result = re.match('(-?[0-9]+)[ ]*<[ ]*\$([0-9]+)[ ]*;', itext[ipos:])
+			if not been_match and re_result:
+				been_match = True
+				obj_index = int(re_result.group(2)) - 1
+				l_op = int(re_result.group(1))
+				obj_record[obj_index].set_min(l_op + 1)
+				ipos = ipos + len(re_result.group(0))
+			
+			re_result = re.match('\$([0-9]+)[ ]*(<)[ ]*\$([0-9]+)[ ]*;', itext[ipos:])
+			if not been_match and re_result:
+				been_match = True
+				rel_stmt = RelStmt()
+				rel_stmt.l_operand = int(re_result.group(1))
+				rel_stmt.r_operand = int(re_result.group(3))
+				rel_stmt.operator = re_result.group(2)
+				rel_record.append(rel_stmt)
+				ipos = ipos + len(re_result.group(0))
+			
+			#case of '>'
+			re_result = re.match('\$([0-9]+)[ ]*>[ ]*(-?[0-9]+)[ ]*;', itext[ipos:])
+			if not been_match and re_result:
+				been_match = True
+				obj_index = int(re_result.group(1)) - 1
+				r_op = int(re_result.group(2))
+				obj_record[obj_index].set_min(r_op + 1)
+				ipos = ipos + len(re_result.group(0))
+			
+			re_result = re.match('(-?[0-9]+)[ ]*>[ ]*\$([0-9]+)[ ]*;', itext[ipos:])
+			if not been_match and re_result:
+				been_match = True
+				obj_index = int(re_result.group(2)) - 1
+				l_op = int(re_result.group(1))
+				obj_record[obj_index].set_max(l_op - 1)
+				ipos = ipos + len(re_result.group(0))
+			
+			re_result = re.match('\$([0-9]+)[ ]*(>)[ ]*\$([0-9]+)[ ]*;', itext[ipos:])
+			if not been_match and re_result:
+				been_match = True
+				rel_stmt = RelStmt()
+				rel_stmt.l_operand = int(re_result.group(1))
+				rel_stmt.r_operand = int(re_result.group(3))
+				rel_stmt.operator = re_result.group(2)
+				rel_record.append(rel_stmt)
+				ipos = ipos + len(re_result.group(0))
+			
+			#case of '<='
+			re_result = re.match('\$([0-9]+)[ ]*<=[ ]*(-?[0-9]+)[ ]*;', itext[ipos:])
+			if not been_match and re_result:
+				been_match = True
+				obj_index = int(re_result.group(1)) - 1
+				r_op = int(re_result.group(2))
+				obj_record[obj_index].set_max(r_op)
+				ipos = ipos + len(re_result.group(0))
+			
+			re_result = re.match('(-?[0-9]+)[ ]*<=[ ]*\$([0-9]+)[ ]*;', itext[ipos:])
+			if not been_match and re_result:
+				been_match = True
+				obj_index = int(re_result.group(2)) - 1
+				l_op = int(re_result.group(1))
+				obj_record[obj_index].set_min(l_op)
+				ipos = ipos + len(re_result.group(0))
+			
+			re_result = re.match('\$([0-9]+)[ ]*(<=)[ ]*\$([0-9]+)[ ]*;', itext[ipos:])
+			if not been_match and re_result:
+				been_match = True
+				rel_stmt = RelStmt()
+				rel_stmt.l_operand = int(re_result.group(1))
+				rel_stmt.r_operand = int(re_result.group(3))
+				rel_stmt.operator = re_result.group(2)
+				rel_record.append(rel_stmt)
+				ipos = ipos + len(re_result.group(0))
+			
+			#case of '>='
+			re_result = re.match('\$([0-9]+)[ ]*>=[ ]*(-?[0-9]+)[ ]*;', itext[ipos:])
+			if not been_match and re_result:
+				been_match = True
+				obj_index = int(re_result.group(1)) - 1
+				r_op = int(re_result.group(2))
+				obj_record[obj_index].set_min(r_op)
+				ipos = ipos + len(re_result.group(0))
+			
+			re_result = re.match('(-?[0-9]+)[ ]*>=[ ]*\$([0-9]+)[ ]*;', itext[ipos:])
+			if not been_match and re_result:
+				been_match = True
+				obj_index = int(re_result.group(2)) - 1
+				l_op = int(re_result.group(1))
+				obj_record[obj_index].set_max(l_op)
+				ipos = ipos + len(re_result.group(0))
+			
+			re_result = re.match('\$([0-9]+)[ ]*(>=)[ ]*\$([0-9]+)[ ]*;', itext[ipos:])
+			if not been_match and re_result:
+				been_match = True
+				rel_stmt = RelStmt()
+				rel_stmt.l_operand = int(re_result.group(1))
+				rel_stmt.r_operand = int(re_result.group(3))
+				rel_stmt.operator = re_result.group(2)
+				rel_record.append(rel_stmt)
+				ipos = ipos + len(re_result.group(0))
+			
+			#case of method 'range'
+			re_result = re.match('\$([0-9]+)\.range\([ ]*(-?[0-9]+)[ ]*,[ ]*(-?[0-9]+)[ ]*\)[ ]*;', itext[ipos:])
+			if not been_match and re_result:
+				been_match = True
+				obj_index = int(re_result.group(1)) - 1
+				para1 = int(re_result.group(2))
+				para2 = int(re_result.group(3))
+				obj_record[obj_index].set_min(para1)
+				obj_record[obj_index].set_max(para2)
+				ipos = ipos + len(re_result.group(0))
+			
+			re_result = re.match('\s+', itext[ipos:])
+			if not been_match and re_result:
+				been_match = True
+				ipos = ipos + len(re_result.group(0))
+		elif part_num == 4:  #handle the 'end-format' section
+			re_result = re.match('"[^"\n]*"', itext[ipos:])  #end-string case
+			if not been_match and re_result:
+				if has_end_str:  #There were more one end-strings.
+					lexical_error = True
+					break
+				else:
+					been_match, has_end_str = True, True
+					end_str = re_result.group(0)
+					ipos = ipos + len(end_str)
+					end_str = end_str[1:len(end_str)-1]
+					end_pos = 0
+					while end_pos < len(end_str):  #analyze the content of the end-string
+						re_result = re.match(r'\\n', end_str[end_pos:])
+						if re_result:
+							end_str_record.append('\n')
+							end_pos = end_pos + 2
+						
+						re_result = re.match(r'\\t', end_str[end_pos:])
+						if re_result:
+							end_str_record.append('\t')
+							end_pos = end_pos + 2
+						
+						re_result = re.match('[ ]+', end_str[end_pos:])
+						if re_result:
+							end_str_record.append(re_result.group(0))
+							end_pos = end_pos + len(re_result.group(0))
+						
+						re_result = re.match('[^\\\\]+', end_str[end_pos:])
+						if re_result:
+							end_str_record.append(re_result.group(0))
+							end_pos = end_pos + len(re_result.group(0))
+			
+			re_result = re.match('\s+', itext[ipos:])
+			if not been_match and re_result:
+				been_match = True
+				ipos = ipos + len(re_result.group(0))
+		
+		if been_match:
+			been_match = False
+		else:
+			lexical_error = True
+			break
+	if lexical_error:
+		break
+	itext = ifile.readline()
+	ilen, ipos = len(itext), 0
+#--analysis stage end--
+
+if lexical_error:
+	print('There was lexical error!')
+else:
+#--output stage--
+	while input_num > 0:
+		input_num = input_num - 1
+#--generation stage--
+		generation_loop = True
+		while generation_loop:
+			for obj in obj_record:
+				if obj._type == 'int':
+					obj.gen_val(level_num)
+			for rel in rel_record:
+				if check_rel(obj_record[rel.l_operand - 1].get_val(), obj_record[rel.r_operand - 1].get_val(), rel.operator):
+					continue
+				else:
+					break
+			else:
+				generation_loop = False
+#--generation stage end--
+		index = 0
+		while index < len(orecord):
+			if type_record[index] == 'int':
+				ofile.write(str(orecord[index].get_val()))
+			else:
+				ofile.write(orecord[index])
+			index = index + 1
+		ofile.write('\n')
+	if len(end_str_record) != 0:
+		for word in end_str_record:
+			ofile.write(word)
+#--output stage end--
+	print('Generate output successfully.')
+
+ifile.close()
+ofile.close()	
---
+++ IG/obj_types.py
@@ -0,0 +1,68 @@
+#This code contains all (build-in) types of variables.
+import random
+
+class VarInt:
+	def __init__(self):
+		self.__val = 0        #defult value
+		self.__max = 2**64    #default max
+		self.__limit_max = None  #the limited max from the original max
+		self.__max_flag = False
+		self.__min = -(2**64) #default min
+		self.__limit_min = None  #the limited min from the original min
+		self.__min_flag = False
+		self.__width = 0      #the width of range
+		self.__base = 10      #base options: 2, 8, 10, 16
+		self._type = 'int'    #read-only
+	def set_max(self, _max):
+		if self.__max_flag:
+			if _max < self.__max:
+				self.__max = _max
+		else:
+			self.__max = _max
+			self.__max_flag = True
+	def set_min(self, _min):
+		if self.__min_flag:
+			if self.__min < _min:
+				self.__min = _min
+		else:
+			self.__min = _min
+			self.__min_flag = True
+	def set_base(self, base):
+		pass
+	def get_val(self):
+		if self.__base == 2:
+			pass
+		elif self.__base == 8:
+			pass
+		elif self.__base == 10:
+			return self.__val
+		elif self.__base == 16:
+			pass
+	def gen_val(self, level):
+		if self.__min < self.__max:
+			if self._gen_count < 10:
+				self.__width = self.__max - self.__min
+				self.__limit_max = int(0 + self.__width ** (level/3))
+				if self.__limit_max > self.__max:
+					self.__limit_max = self.__max
+				self.__limit_min = int(0 - self.__width ** (level/3))
+				if self.__limit_min < self.__min:
+					self.__limit_min = self.__min
+				self.__val = random.randint(self.__limit_min, self.__limit_max)
+				self._gen_count = self._gen_count + 1
+				return True
+			else:
+				self.reset()
+		else:
+			return False
+	def reset(self):
+		self._gen_count = 0
+	def prime(self):  #generate a prime number value
+		pass
+
+class RelStmt:  #this class handles relation statements
+	def _init_(self):
+		self.l_operand = None #left object number
+		self.r_operand = None #right object number
+		self.operator = None  #operator type
+		
