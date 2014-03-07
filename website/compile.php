3784b63003386ccd84a6066128157b4774664c64
kid9917002 <kid9917002@gmail.com>
2014-03-08 01:26:02 +0800

    Update compile.php
    Change mode to fix "cannot create AC program" BUGs.

--- website/compile.php
+++ website/compile.php
@@ -4,6 +4,7 @@
    $codepath = "/home/coding/OG/problist/code_file/".$j."/";
    $ACpath = "/home/coding/OG/problist/DB/".$j."/";
    if (!file_exists($ACpath)) {  mkdir($ACpath); }
+   chmod($ACpath,0775);
    if(scan($codepath)!=2 && scan($ACpath) < scan($codepath)){
         $ACcmd = "python3.3 /home/coding/OG/autocompiler.py ".$j." ".$codepath." ".$ACpath;
         //echo $ACcmd;
