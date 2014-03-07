a69789d7523eadc7e1c4fdae61dd223f876ffe10
kid9917002 <kid9917002@gmail.com>
2014-03-07 23:46:56 +0800

    Update compile.php

--- website/compile.php
+++ website/compile.php
@@ -4,7 +4,7 @@
    $codepath = "/home/coding/OG/problist/code_file/".$j."/";
    $ACpath = "/home/coding/OG/problist/DB/".$j."/";
    if (!file_exists($ACpath)) {  mkdir($ACpath); }
-   if(scan($ACpath)==2 && scan($codepath)!=2 && scan($ACpath) < scan($codepath)){
+   if(scan($codepath)!=2 && scan($ACpath) < scan($codepath)){
         $ACcmd = "python3.3 /home/coding/OG/autocompiler.py ".$j." ".$codepath." ".$ACpath;
         //echo $ACcmd;
         $o = shell_exec($ACcmd);
